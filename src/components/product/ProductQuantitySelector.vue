f30f9f089db391f57a581b501e4190478d7b99a4
Danillo Mota <danillomota99@gmail.com>
2025-04-09 17:30:05 -0300

    adjust mobile screen responsiveness

--- src/components/product/ProductQuantitySelector.vue
+++ src/components/product/ProductQuantitySelector.vue
@@ -1,29 +1,29 @@
 <template>
-  <div class="flex flex-col sm:flex-row items-start gap-2 w-full">
+  <div class="flex flex-col sm:flex-row gap-4">
     <!-- Contador de Quantidade -->
-    <div class="w-full sm:w-[150px] h-[73.31px]"> <!-- Aumentado de 94.28px para 150px -->
+    <div class="w-full sm:w-[150px] h-[45px] sm:h-[73.31px]">
       <div class="flex flex-row justify-between items-center h-full border-2 border-black bg-white">
         <button 
           @click="decrementQuantity" 
-          class="flex items-center justify-center w-[40px] h-full hover:bg-black/5"
+          class="flex items-center justify-center w-[30px] sm:w-[40px] h-full hover:bg-black/5"
           :aria-label="$t('cart.decrease')"
         >
-          <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
+          <svg class="w-3 h-3 sm:w-4 sm:h-4" viewBox="0 0 16 16" fill="none">
             <path d="M3.33337 8H12.6667" stroke="#1E1E1E" stroke-width="1.6"/>
           </svg>
         </button>
         
-        <div class="font-archivo font-bold text-[22px] text-[#010101] flex items-center justify-center"
+        <div class="font-archivo font-bold text-base sm:text-[22px] text-[#010101] flex items-center justify-center"
              :aria-label="$t('cart.quantity')">
           {{ quantity }}
         </div>
         
         <button 
           @click="incrementQuantity" 
-          class="flex items-center justify-center w-[40px] h-full hover:bg-black/5"
+          class="flex items-center justify-center w-[30px] sm:w-[40px] h-full hover:bg-black/5"
           :aria-label="$t('cart.increase')"
         >
-          <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
+          <svg class="w-3 h-3 sm:w-4 sm:h-4" viewBox="0 0 16 16" fill="none">
             <path d="M8 3.33337V12.6667M3.33337 8H12.6667" stroke="#1E1E1E" stroke-width="1.6"/>
           </svg>
         </button>
@@ -32,10 +32,10 @@
 
     <!-- Botão Add Cart -->
     <button 
-      class="w-full sm:flex-1 h-[73.31px] bg-black flex justify-center items-center"
+      class="w-full sm:flex-1 h-[45px] sm:h-[73.31px] bg-black flex justify-center items-center"
       @click="addToCart"
     >
-      <span class="font-archivo-narrow font-semibold text-[34px] leading-[72px] text-[#FFDD00]">
+      <span class="font-archivo-narrow font-semibold text-[#FFDD00]">
         {{ $t('cart.addToCart') }}
       </span>
     </button>
